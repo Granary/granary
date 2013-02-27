fed6c7bc107bc1457f61f2cdee02ed6259b9ff9d
pgoodman <peter.goodman@gmail.com>
2013-02-26 22:50:14 -0500

    Manually added a redundant centinel into a call to execle.

--- granary/user/posix/wrappers.h
+++ granary/user/posix/wrappers.h
@@ -130,7 +130,8 @@
         (void) last_arg;
         return execle(
             __path, args[0], args[1], args[2], args[3], args[4],
-            args[5], args[6], args[7], args[8], args[9], args[10]
+            args[5], args[6], args[7], args[8], args[9], args[10],
+            nullptr
         );
     })
 #endif
