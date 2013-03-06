40b8ec2cc557cc023dc20d5379b136e53cad6466
pgoodman <peter.goodman@gmail.com>
2013-03-06 11:56:48 -0500

    QUick update to the custom wrappers; seems that I left sigaction stubbed out, which might be the cause of some instrumentation issues, at least with clang on Linux. Not sure yet, will test.

--- granary/user/posix/wrappers.h
+++ granary/user/posix/wrappers.h
@@ -173,37 +173,32 @@
         va_start(args__, _arg3);
         uint64_t _arg4 = va_arg(args__, uint64_t); // union semun
         va_end(args__);
-        int ret = semctl(_arg1, _arg2, _arg3, _arg4);
+        return semctl(_arg1, _arg2, _arg3, _arg4);
+    })
+#endif
+
+
+#ifdef CAN_WRAP_open
+#   define WRAPPER_FOR_open
+    FUNCTION_WRAPPER(open, (int), (const char *_arg1, int _arg2, ...), {
+        va_list args__;
+        va_start(args__, _arg2);
+        mode_t _arg3 = va_arg(args__, int);
+        va_end(args__);
+        int ret = open(_arg1, _arg2, _arg3);
+
+        granary::printf("function_wrapper(open, %s, %d) -> %d\n", _arg1, _arg2, ret);
+
         return ret;
     })
 #endif
 
 
-#define WRAPPER_FOR_open
-FUNCTION_WRAPPER(open, (int), (const char *_arg1, int _arg2, ...), {
-    va_list args__;
-    va_start(args__, _arg2);
-    mode_t _arg3 = va_arg(args__, int);
-    va_end(args__);
-    int ret = open(_arg1, _arg2, _arg3);
+#define WRAP_DEBUG 1
 
-    granary::printf("function_wrapper(open, %s, %d) -> %d\n", _arg1, _arg2, ret);
-
-    return ret;
-})
+#if WRAP_DEBUG
 
 
-#if 0
-#define WRAPPER_FOR_pthread_create
-FUNCTION_WRAPPER(pthread_create, (int), (pthread_t *thread, const pthread_attr_t *attr,
-                          void *(*start_routine) (void *), void *arg), {
-
-})
-#endif
-
-
-
-#if 1
 #ifdef CAN_WRAP_pread
 #   define WRAPPER_FOR_pread
     FUNCTION_WRAPPER(pread, (ssize_t), (int fd, void *buf, size_t count, off_t offset), {
@@ -211,53 +206,57 @@ FUNCTION_WRAPPER(pthread_create, (int), (pthread_t *thread, const pthread_attr_t
         return pread(fd, buf, count, offset);
     })
 #endif
-#define WRAPPER_FOR_lseek
-FUNCTION_WRAPPER(lseek, (off_t), (int fildes, off_t offset, int whence), {
-    granary::printf("function_wrapper(lseek, %d, %lu, %d)\n", fildes, offset, whence);
-    return lseek(fildes, offset, whence);
-})
-
-#define WRAPPER_FOR_close
-FUNCTION_WRAPPER(close, (int), (int fd), {
-    int ret(close(fd));
-    granary::printf("function_wrapper(close, %d) -> %d\n", fd, ret);
-    return ret;
-})
-
-#define WRAPPER_FOR_write
-FUNCTION_WRAPPER(write, (size_t), (int fildes, const void *buf, size_t nbytes), {
-    granary::printf("function_wrapper(write, %d, %p, %lu)\n", fildes, buf, nbytes);
-    return write(fildes, buf, nbytes);
-})
 
 
-
-#define WRAPPER_FOR_fopen
-FUNCTION_WRAPPER(fopen, (FILE *), (const char *_arg1, const char *_arg2), {
-    granary::printf("function_wrapper(fopen, %s, %s)\n", _arg1, _arg2);
-    FILE *ret = fopen(_arg1, _arg2);
-    RETURN_WRAP(ret);
-    return ret;
-})
-
-#if 0
-#define WRAPPER_FOR_free
-FUNCTION_WRAPPER(free, (void), (void *addr), {
-    granary::printf("function_wrapper(free, %p)\n", addr);
-})
+#ifdef CAN_WRAP_lseek
+#   define WRAPPER_FOR_lseek
+    FUNCTION_WRAPPER(lseek, (off_t), (int fildes, off_t offset, int whence), {
+        granary::printf("function_wrapper(lseek, %d, %lu, %d)\n", fildes, offset, whence);
+        return lseek(fildes, offset, whence);
+    })
 #endif
 
-#define WRAPPER_FOR_getenv
-FUNCTION_WRAPPER(getenv, (char *), (const char *arg1), {
-    char *ret(getenv(arg1));
-    granary::printf("function_wrapper(getenv, %s) -> %s\n", arg1, ret);
-    return ret;
-})
 
-
-#define WRAPPER_FOR_sigaction
-FUNCTION_WRAPPER(sigaction, (int), (int, const struct sigaction * , struct sigaction * ), {
-    return 0;
-})
+#ifdef CAN_WRAP_close
+#   define WRAPPER_FOR_close
+    FUNCTION_WRAPPER(close, (int), (int fd), {
+        int ret(close(fd));
+        granary::printf("function_wrapper(close, %d) -> %d\n", fd, ret);
+        return ret;
+    })
 #endif
+
+
+#ifdef CAN_WRAP_write
+#   define WRAPPER_FOR_write
+    FUNCTION_WRAPPER(write, (size_t), (int fildes, const void *buf, size_t nbytes), {
+        granary::printf("function_wrapper(write, %d, %p, %lu)\n", fildes, buf, nbytes);
+        return write(fildes, buf, nbytes);
+    })
+#endif
+
+
+#ifdef CAN_WRAP_fopen
+#   define WRAPPER_FOR_fopen
+    FUNCTION_WRAPPER(fopen, (FILE *), (const char *_arg1, const char *_arg2), {
+        granary::printf("function_wrapper(fopen, %s, %s)\n", _arg1, _arg2);
+        FILE *ret = fopen(_arg1, _arg2);
+        RETURN_WRAP(ret);
+        return ret;
+    })
+#endif
+
+
+#ifdef CAN_WRAP_getenv
+#   define WRAPPER_FOR_getenv
+    FUNCTION_WRAPPER(getenv, (char *), (const char *arg1), {
+        char *ret(getenv(arg1));
+        granary::printf("function_wrapper(getenv, %s) -> %s\n", arg1, ret);
+        return ret;
+    })
+#endif
+
+
+#endif /* WRAP_DEBUG */
+
 #endif /* granary_USER_POSIX_OVERRIDE_WRAPPERS_H_ */
