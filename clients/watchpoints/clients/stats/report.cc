8ac4fc745f3450e2b71511e1f1d873bc999f0a2a
pgoodman <peter.goodman@gmail.com>
2013-07-18 23:32:49 -0400

    Added an extra recorded stat.

--- clients/watchpoints/clients/stats/report.cc
+++ clients/watchpoints/clients/stats/report.cc
@@ -22,6 +22,7 @@ namespace client {
     void report(void) throw() {
 
         uint64_t num_bbs(0);
+        uint64_t num_bbs_with_watched_ops(0);
         uint64_t num_executed_bbs(0);
         uint64_t num_static_ops(0);
         uint64_t num_dynamic_ops(0);
@@ -34,18 +35,29 @@ namespace client {
             num_executed_bbs += bb->num_executions;
             num_dynamic_ops += bb->num_executions * bb->num_memory_ops;
             num_watched_ops += bb->num_watched_memory_ops;
+
+            if(bb->num_watched_memory_ops) {
+                num_bbs_with_watched_ops++;
+            }
         }
 
         printf("\nWatchpoint Statistics\n");
+
         printf("Number of basic blocks: %lu\n",
             num_bbs);
+
+        printf("Number of basic blocks with watched memory operations: %lu\n",
+            num_bbs_with_watched_ops);
+
         printf("Number of executed basic blocks: %lu\n",
             num_executed_bbs);
 
         printf("Number of static memory operations: %lu\n",
             num_static_ops);
+
         printf("Number of dynamic memory operations: %lu\n",
             num_dynamic_ops);
+
         printf("Number of watched memory operators: %lu\n",
             num_watched_ops);
     }
