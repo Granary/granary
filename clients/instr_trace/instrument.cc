0117aa26053a0ef97863a70d81f0c18979e33971
Tunji Ruwase <tjruwase@gmail.com>
2013-07-18 23:21:19 -0400

    Pass most of the trace record arguments

--- clients/instr_trace/instrument.cc
+++ clients/instr_trace/instrument.cc
@@ -12,14 +12,14 @@ namespace client {
   #define NUM_TRACE_RECORDS 1024
   static app_pc EVENT_TRACE_INSTR = nullptr;
 
-  __attribute__((hot)) void trace_instruction (basic_block_state *) throw() {
+  __attribute__((hot)) void trace_instruction (app_pc pc, unsigned opcode, uint64_t opnd1, uint64_t opnd2) throw() {
     cpu_state_handle cpu;
     trace_record *log = &cpu->log[cpu->log_idx % NUM_TRACE_RECORDS];
 
-    log->opcode = dynamorio::OP_INVALID;
-    log->pc = 0; 
-    log->opnd1 = 0;
-    log->opnd2 = 0;
+    log->opcode = opcode;
+    log->pc = pc; 
+    log->opnd1 = opnd1;
+    log->opnd2 = opnd2;
 
     cpu->stats.num_instrs++;
     cpu->log_idx++;
@@ -40,7 +40,10 @@ namespace client {
 			   reinterpret_cast<void *>(&num_cpus));
     printf("[instr_trace] Finished initializing %d cpus \n", num_cpus);
   }
-  
+
+  static void load_operand (const operand_ref &, instruction_list &, instruction &) throw() {
+  }
+
   granary::instrumentation_policy instr_trace_policy::visit_app_instructions
   (
    granary::cpu_state_handle,
@@ -51,7 +54,12 @@ namespace client {
     
     for(instruction in(ls.first()); in.is_valid(); in = in.next()) {
       instruction x(ls.insert_before(in, label_()));
-      insert_clean_call_after(ls, x, EVENT_TRACE_INSTR, nullptr);
+      x = ls.insert_after(x, push_(reg::arg3));
+      x = ls.insert_after(x, push_(reg::arg4));
+      in.for_each_operand(load_operand, ls, x);
+      x = insert_clean_call_after(ls, x, EVENT_TRACE_INSTR, in.pc(), static_cast<uint64_t>(in.op_code()));
+      x = ls.insert_after(x, pop_(reg::arg4));
+      x = ls.insert_after(x, pop_(reg::arg3));
     }
     
     return granary::policy_for<instr_trace_policy>();
