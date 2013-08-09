d455f9c538cc0b292cb041b6eb0ba8f0d55f7f37
Peter Goodman <pag@cs.toronto.edu>
2013-08-09 14:18:17 -0400

    Removing duplicate wrapper definitions.

--- clients/watchpoints/clients/shadow_memory/kernel/linux/wrappers.h
+++ clients/watchpoints/clients/shadow_memory/kernel/linux/wrappers.h
@@ -15,7 +15,6 @@ namespace client {
     extern void report(void);
 }
 
-//extern void client::report(void);
 
 #ifndef APP_WRAPPER_FOR_pointer
 #   define APP_WRAPPER_FOR_pointer
@@ -67,89 +66,6 @@ namespace client {
         }
     })
 #endif
-
-
-#define WP_BASIC_POINTER_WRAPPER(base_type) \
-    TYPE_WRAPPER(base_type *, { \
-        NO_PRE \
-        NO_POST \
-        NO_RETURN \
-    })
-
-
-#ifndef APP_WRAPPER_FOR_void_pointer
-#   define APP_WRAPPER_FOR_void_pointer
-    WP_BASIC_POINTER_WRAPPER(void)
-#endif
-
-
-#ifndef APP_WRAPPER_FOR_int8_t_pointer
-#   define APP_WRAPPER_FOR_int8_t_pointer
-    static_assert(sizeof(char) == sizeof(int8_t), "Type size mismatch.");
-    WP_BASIC_POINTER_WRAPPER(char)
-#endif
-
-
-#ifndef APP_WRAPPER_FOR_uint8_t_pointer
-#   define APP_WRAPPER_FOR_uint8_t_pointer
-    static_assert(sizeof(char) == sizeof(int8_t), "Type size mismatch.");
-    WP_BASIC_POINTER_WRAPPER(unsigned char)
-#endif
-
-
-#ifndef APP_WRAPPER_FOR_int16_t_pointer
-#   define APP_WRAPPER_FOR_int16_t_pointer
-    static_assert(sizeof(short) == sizeof(int16_t), "Type size mismatch.");
-    WP_BASIC_POINTER_WRAPPER(short)
-#endif
-
-
-#ifndef APP_WRAPPER_FOR_uint16_t_pointer
-#   define APP_WRAPPER_FOR_uint16_t_pointer
-    static_assert(sizeof(short) == sizeof(int16_t), "Type size mismatch.");
-    WP_BASIC_POINTER_WRAPPER(unsigned short)
-#endif
-
-
-#ifndef APP_WRAPPER_FOR_int32_t_pointer
-#   define APP_WRAPPER_FOR_int32_t_pointer
-    static_assert(sizeof(int) == sizeof(int32_t), "Type size mismatch.");
-    WP_BASIC_POINTER_WRAPPER(int)
-#endif
-
-
-#ifndef APP_WRAPPER_FOR_uint32_t_pointer
-#   define APP_WRAPPER_FOR_uint32_t_pointer
-    static_assert(sizeof(int) == sizeof(int32_t), "Type size mismatch.");
-    WP_BASIC_POINTER_WRAPPER(unsigned)
-#endif
-
-
-#ifndef APP_WRAPPER_FOR_int64_t_pointer
-#   define APP_WRAPPER_FOR_int64_t_pointer
-    static_assert(sizeof(long) == sizeof(int64_t), "Type size mismatch.");
-    WP_BASIC_POINTER_WRAPPER(long)
-#endif
-
-
-#ifndef APP_WRAPPER_FOR_uint64_t_pointer
-#   define APP_WRAPPER_FOR_uint64_t_pointer
-    static_assert(sizeof(long) == sizeof(int64_t), "Type size mismatch.");
-    WP_BASIC_POINTER_WRAPPER(unsigned long)
-#endif
-
-
-#ifndef APP_WRAPPER_FOR_float_pointer
-#   define APP_WRAPPER_FOR_float_pointer
-    WP_BASIC_POINTER_WRAPPER(float)
-#endif
-
-
-#ifndef APP_WRAPPER_FOR_double_pointer
-#   define APP_WRAPPER_FOR_double_pointer
-    WP_BASIC_POINTER_WRAPPER(double)
-#endif
-
 
 
 #endif /* WRAPPER_H_ */
